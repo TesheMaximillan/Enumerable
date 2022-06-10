aa5a251f9abd1b53cf2e9532b2384a48c0c00e98
buhariaminat <buhariaminat@yahoo.com>
2022-06-10 23:08:26 +0100

    Add enumerable

---
+++ my_enumerable.rb
@@ -0,0 +1,16 @@
+def all?
+    each { |list| return false unless yield(list) }
+    true
+  end
+
+  def any?
+    each { |list| return true if yield(list) }
+    false
+  end
+
+  def filter
+    filtered = []
+    each { |list| filtered << list if yield(list) }
+    filtered
+  end
+end
